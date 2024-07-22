02c4c6f5e8de65b4fb224ab88279688597703e58
seohyg <162432409+seohyg@users.noreply.github.com>
2024-07-22 14:49:03 +0900

    Update sol_act_test.py

--- solenoid_test/sol_act_test.py
+++ solenoid_test/sol_act_test.py
@@ -1,9 +1,9 @@
 import gpiod
 import time
-LED_PIN = 17   #6번째 핀(GND는 5번째 핀)
+LED_PIN = 17
 chip = gpiod.Chip('gpiochip4')
 led_line = chip.get_line(LED_PIN)
-led_line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
+led_line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT) #이름만 led로 조작
 try:
    # for _ in range(10):
    while True:
